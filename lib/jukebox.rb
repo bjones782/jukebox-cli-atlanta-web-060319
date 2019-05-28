0319a055389b6d8beeb8226abacc69b9e4041509
Brooke Jones <bjones782@gmail.com>
2019-05-28 19:22:36 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -48,3 +48,29 @@ def exit_jukebox
   puts "Goodbye"
 end	
 
+def run(my_songs)
+  #this method is the same as in jukebox.rb	  #this method is the same as in jukebox.rb
+end	  help
+  puts "Please enter a command:"
+  user_response = gets.chomp
+  while user_response != "exit"
+    case user_response
+      when "help"
+        help
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      when "list"
+        list(my_songs)
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      when "play"
+        play(my_songs)
+        puts "Please enter a command:"
+        user_response = gets.chomp
+      when "exit"
+        exit_jukebox
+      else
+        puts "Invalid input, please try again:"
+        user_response = gets.chomp
+      end
+    end
